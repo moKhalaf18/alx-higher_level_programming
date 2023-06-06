f09b5f087e3b3adc7880f9f20b3ef3c1c916d6b2
mohamed khalaf <mohamedkhallaf654@gmail.com>
2023-06-06 14:57:47 -0700

    a

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
-for i in range(ord('a'), ord('z') + 1):
-    if(i != ord('e') and i != ord('q')):
-        print("{:c}".format(i), end='')
+# Author - Bamidele Adefolaju
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-def islower(c):
-    return True if (ord(c) >= ord('a') and ord(c) <= ord('z')) else False
+# Author - Bamidele Adefolaju
+
+def uppercase(str):
+    """Print a string in uppercase."""
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
